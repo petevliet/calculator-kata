0f50dcbd18b377dbddfd4c70efb0e6e80321885d
Peter Vliet <plvliet@us.ibm.com>
2016-02-24 10:31:51 -0800

    calc can add string arguments

--- src/calculator.js
+++ src/calculator.js
@@ -1,3 +1,11 @@
-export function calculator(num) {
-  return 'hello';
+import Immutable from 'immutable';
+import {List} from 'immutable';
+
+export function add(str) {
+  if (!str) return 0;
+
+  const stringList = Immutable.fromJS(str.split(','));
+  const intList = stringList.map(numStr => parseInt(numStr));
+
+  return intList.reduce( (prev, cur) => prev + cur);
 };
--- test/calculator_spec.js
+++ test/calculator_spec.js
@@ -1,11 +1,18 @@
 import {expect} from 'chai';
 
-import {calculator} from '../src/calculator';
+import {add} from '../src/calculator';
 
 describe('Calculator', () => {
 
-  it('returns hello', () => {
-      expect(calculator()).to.equal('hello');
+  describe('add', () => {
+    it('returns 0 for empty string', () => {
+      expect(add("")).to.equal(0);
+    });
+
+    it('returns sum of values in string', () => {
+      expect(add("1")).to.equal(1);
+      expect(add("1,2,3,4,5")).to.equal(15);
+    });
   });
 
 });
